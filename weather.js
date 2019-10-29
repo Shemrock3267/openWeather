b4c3304e53cddbd1afb4cfe43e0e43dac405a9bc
Roman <shafty17@gmail.com>
2019-10-29 17:30:32 +0200

    Update weather.js

--- weather.js
+++ weather.js
@@ -7,7 +7,7 @@ class Weather {
   // Fetch weather from API
   async fetchWeather() {
     const response = await fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=${this.city}&APPID=${
+      `https://api.openweathermap.org/data/2.5/weather?q=${this.city}&APPID=${
         this.apiKey
       }&units=metric`
     );
